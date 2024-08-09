9f66a8ee7e3cd28d1ba256393976bf366d80d49c
Ranadeep Ajay <ranadeepajay@gmail.com>
2024-08-10 01:56:55 +0530

    authentication middleware changes

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login,logout
+
+from .middlewares import auth,guest
 # Create your views here.
 
-
+@guest
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -16,6 +18,7 @@ def login_view(request):
         form = AuthenticationForm(initial=initial_data)
     return render(request, 'auth/login.html',{'form':form})
 
+@guest
 def register_view(request):
     if request.method=='POST':
         form=UserCreationForm(request.POST)
@@ -36,5 +39,6 @@ def logout_view(request):
     logout(request)
     return redirect('login')
 
+@auth
 def dashboard_view(request):
     return render(request, 'dashboard.html')
